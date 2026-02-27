582fb8c4ce54ea3bc62644e111b3635e5aa1a85a
jayagrawal-petabyte <jayagrawal72511@gmail.com>
2026-02-27 15:34:34 +0530

    harcoded input

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,5 +4,23 @@ public class PalindromeCheckerApp {
         System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version: 1.0");
         System.out.println("System initialized successfully.");
+
+        // Hardcoded input string
+        String input = "madan";
+
+        boolean isPalindrome = false;
+
+        // Loop only till half of the string length
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) == input.charAt(input.length() - 1 - i)) {
+                isPalindrome = true;
+                break;
+            }
+        }
+
+        // Display output
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome?: ");
+        System.out.println(isPalindrome);
     }
 }
