cc95a28e091926fdaf355c0a187b4b10f4fb6068
jayagrawal-petabyte <jayagrawal72511@gmail.com>
2026-03-09 15:51:05 +0530

    UC9 recurrsive call

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,29 +1,25 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
-        String input = "level";
+        String input = "sadas";
 
-        LinkedList<Character> list = new LinkedList<>();
-
-        for (char c : input.toCharArray()) {
-            list.add(c);
-        }
-
-        boolean isPalindrome = true;
-
-        while (list.size() > 1) {
-
-            char first = list.removeFirst();
-            char last = list.removeLast();
-
-            if (first != last) {
-                isPalindrome = false;
-                break;
-            }
-        }
+        boolean isPalindrome = checkPalindrome(input, 0, input.length() - 1);
 
         System.out.println("Input: " + input);
         System.out.println("Is Palindrome?");
         System.out.println(isPalindrome);
+    }
+
+    private static boolean checkPalindrome(String str, int start, int end) {
+
+        if (start >= end) {
+            return true;
+        }
+
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+
+        return checkPalindrome(str, start + 1, end - 1);
     }
 }
