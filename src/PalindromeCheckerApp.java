b8a532e75bb0c9998a3d3d4ee23bfc46867e537b
jayagrawal-petabyte <jayagrawal72511@gmail.com>
2026-03-16 15:18:06 +0530

    UC12: strategy planning

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,35 +1,83 @@
-class PalindromeChecker {
+import java.util.*;
 
-    public boolean checkPalindrome(String input) {
+// Strategy Interface
+interface PalindromeStrategy {
+    boolean check(String input);
+}
 
-        int start = 0;
-        int end = input.length() - 1;
+// Stack Strategy Implementation
+class StackStrategy implements PalindromeStrategy {
 
-        while (start < end) {
+    public boolean check(String input) {
 
-            if (input.charAt(start) != input.charAt(end)) {
+        Stack<Character> stack = new Stack<>();
+
+        // Push characters into stack
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+        // Compare characters while popping
+        for (char c : input.toCharArray()) {
+            if (stack.pop() != c) {
                 return false;
             }
-
-            start++;
-            end--;
         }
 
         return true;
     }
 }
 
-public class PalindromeCheckerApp {
+// Deque Strategy Implementation
+class DequeStrategy implements PalindromeStrategy {
+
+    public boolean check(String input) {
+
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char c : input.toCharArray()) {
+            deque.addLast(c);
+        }
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+// Main Application Class
+public class UseCase12PalindromeCheckerApp {
+
+    private PalindromeStrategy strategy;
+
+    // Inject strategy at runtime
+    public UseCase12PalindromeCheckerApp(PalindromeStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public boolean checkPalindrome(String input) {
+        return strategy.check(input);
+    }
 
     public static void main(String[] args) {
 
-        String input = "level";
+        Scanner scanner = new Scanner(System.in);
 
-        PalindromeChecker checker = new PalindromeChecker();
+        System.out.println("Enter a string:");
+        String input = scanner.nextLine();
 
-        boolean result = checker.checkPalindrome(input);
+        // Choose strategy (StackStrategy or DequeStrategy)
+        PalindromeStrategy strategy = new StackStrategy();
 
-        System.out.println("Input: " + input);
-        System.out.println("Is Palindrome?: " + result);
+        UseCase12PalindromeCheckerApp app =
+                new UseCase12PalindromeCheckerApp(strategy);
+
+        boolean result = app.checkPalindrome(input);
+
+        System.out.println("Is Palindrome: " + result);
     }
 }
